e3cde022dfb81897e232ec2308216dfb893b7c4e
Re1gns <lreigns12@gmail.com>
2023-11-28 19:34:08 +0100

    HomePage Feat

--- GymManagementSystem/main/models.py
+++ GymManagementSystem/main/models.py
@@ -1,6 +1,25 @@
 from django.db import models
+from django.utils.html import mark_safe
 
-# Create your models here.
+#Banners Model
+class Banners(models.Model):
+    img = models.ImageField(upload_to="banners/")
+    alt_text = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.alt_text
+    
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="80" />' % (self.img.url))
+
+# Services Model
 class Service(models.Model):
     title=models.CharField(max_length=150)
     details=models.TextField()
+    img = models.ImageField(upload_to="services/", null=True)
+
+    def __str__(self):
+        return self.title
+    
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="80" />' % (self.img.url))
--- GymManagementSystem/main/views.py
+++ GymManagementSystem/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 #Home Page
 def home(request):
-    return render(request, 'home.html')
+    banners=models.Banners.objects.all()
+    services=models.Service.objects.all()[:3]
+    return render(request, 'home.html', {'banners':banners, 'services':services})
